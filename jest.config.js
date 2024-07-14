b626b014859d12aeed76fa4e464a201f1611621e
mielk <mielk@o2.pl>
2024-07-14 02:01:22 +0200

    Configuration changes to allow smooth unit testing.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 export default {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	// roots: ['<rootDir>/tests'], // Specify the root directory for Jest to look for tests
+	roots: ['<rootDir>/tests'], // Specify the root directory for Jest to look for tests
 	transform: {
 		'^.+\\.(ts|tsx)$': 'ts-jest',
 		'^.+\\.(js|jsx|mjs)$': 'babel-jest',
@@ -11,9 +11,9 @@ export default {
 			tsconfig: '<rootDir>/tsconfig.json',
 		},
 	},
-	// testMatch: [
-	// 	'**/?(*.)+(spec|test).[jt]s?(x)', // Match test files with .spec.ts or .test.ts extensions
-	// ],
+	testMatch: [
+		'**/?(*.)+(spec|test).[jt]s?(x)', // Match test files with .spec.ts or .test.ts extensions
+	],
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'], // File extensions for modules
 	transformIgnorePatterns: ['/node_modules/(?!mielk-fn|mielk-db).+\\.js$'],
 	reporters: [
